e4e6b02e52eac39bf2ff58f74d4638bdf88785c2
Nisfeight8 <fanispapazoglou1999@gmail.com>
2025-04-04 16:58:00 +0300

    Fix init sww script

--- nisfere/scripts/init-swww.sh
+++ nisfere/scripts/init-swww.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-THEMES_DIR="$HOME/.config/nisfere/themes/"
+USER_THEMES_DIR="$HOME/.config/nisfere/themes"
+NISFERE_THEMES_DIR="$HOME/.nisfere/themes"
 CURRENT_THEME_FILE="$HOME/.nisfere/current_theme.txt"
 CURRENT_THEME=$(cat $CURRENT_THEME_FILE)
-WALLPAPER_FILE="$THEMES_DIR/$CURRENT_THEME/wallpaper.png"
+
+# Load themes directory correctly
+if [[ -d "$USER_THEMES_DIR/$CURRENT_THEME" ]]; then
+  THEME_PATH="$USER_THEMES_DIR/$CURRENT_THEME"
+elif [[ -d "$NISFERE_THEMES_DIR/$CURRENT_THEME" ]]; then
+  THEME_PATH="$NISFERE_THEMES_DIR/$CURRENT_THEME"
+else
+  echo "Error: Theme '$CURRENT_THEME' not found!"
+  exit 1
+fi
+
+WALLPAPER_FILE="$THEME_PATH/wallpaper.png"
 # Check swww daemon
 swww query
 if [ $? -eq 1 ]; then
