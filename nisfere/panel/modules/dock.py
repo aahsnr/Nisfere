97328ae468535940e9c895503ec9582a5887a80c
Nisfeight8 <fanispapazoglou1999@gmail.com>
2025-04-20 17:31:20 +0300

    Dock group clients

--- nisfere/panel/modules/dock.py
+++ nisfere/panel/modules/dock.py
@@ -2,40 +2,57 @@ from fabric.widgets.box import Box
 from fabric.widgets.label import Label
 from fabric.widgets.image import Image
 from fabric.widgets.wayland import WaylandWindow as Window
-from fabric.utils.helpers import exec_shell_command_async
+from fabric.utils.helpers import exec_shell_command_async, truncate
 
 from services import hyprland_clients_service, HyprlandClient
-from shared import Button
+from shared import Button, PopOverWindow
 from utils.config import CONFIG
+from utils.icons import close as close_icon
+
 from gi.repository import GLib
 
 config = CONFIG['dock-config']
-
-default_orientation = "h" if config['position'] in ("top", "bottom") else "v"
-
+position = config['position']
+default_orientation = "h" if position in ("top", "bottom") else "v"
 
 class Dock(Window):
     def __init__(self, **kwargs):
         super().__init__(
             layer="overlay",
-            anchor=f"{config['position']} center",
+            anchor=f"{position} center",
             exclusivity="none",
             **kwargs,
         )
 
         self.is_hidden = False
-        self.hide_id = None  # Timeout reference
+        self.hide_id = None
         self.should_hide = False
+        self.popup = DockPopup(parent=self,pointing_to=self)
 
-        # Pinned apps box
         self.pinned_box = Box(
             orientation=default_orientation,
             spacing=13,
-            children=[PinnedDockButton(app=app)
-                      for app in config['pinned_apps']]
+            children=[PinnedDockButton(app=app) for app in config['pinned_apps']]
         )
 
-        # Initialize the service and listen to signals
+        self.clients_box = Box(
+            orientation=default_orientation,
+            spacing=13,
+        )
+
+        self.client_buttons = {}
+
+        self.inner_box = Box(
+            orientation=default_orientation,
+            spacing=13,
+            children=[self.pinned_box, self.clients_box]
+        )
+
+        self.connect("enter-notify-event", lambda *args: self.on_hover_enter())
+        self.connect("leave-notify-event", lambda *args: self.on_hover_leave())
+
+        self.add(self.inner_box)
+
         self.clients = hyprland_clients_service.build()\
             .connect('initialized', lambda *args: self.on_initialized())\
             .connect('client-added', lambda _, client: self.on_client_added(client))\
@@ -44,189 +61,220 @@ class Dock(Window):
             .connect('filled-workspace', lambda *args: self.on_filled_workspace())\
             .unwrap()
 
-        # Main clients box (to hold active apps)
-        self.clients_box = Box(
-            orientation=default_orientation,
-            spacing=13,
-        )
-
-        # Inner box for layout
-        self.inner_box = Box(
-            orientation=default_orientation,
-            spacing=13,
-            # style_classes="menu",
-            children=[self.pinned_box, self.clients_box]
-        )
-
-        self.connect("enter-notify-event", lambda *args: self.on_hover_enter())
-        self.connect("leave-notify-event", lambda *args: self.on_hover_leave())
-
-        self.add(self.inner_box)
-        # Trigger the initialization
         self.clients.emit("initialized")
-
-        self.show_all()
-
-    def on_empty_workspace(self):
-        """Show the dock when workspace is empty."""
-        self.should_hide = False
-        self.toggle_dock(show=True)
-
-    def on_filled_workspace(self):
-        """Delay hiding the dock when workspace is filled."""
-        self.should_hide = True
-        self.delay_hide()
-
-    def on_hover_enter(self):
-        """Show the dock when hovered."""
-        self.toggle_dock(show=True)
-
-    def on_hover_leave(self):
-        """Delay hiding the dock when mouse leaves."""
-        if self.should_hide:
-            self.delay_hide()
+        self.show()
 
     def on_initialized(self):
-        """Creates all clients in the dock."""
         for client in self.clients.clients:
             self.on_client_added(client)
 
     def on_client_added(self, client: HyprlandClient):
-        """Creates a widget for each individual client, including replacing pinned ones if needed."""
-        # If it's a pinned app, we should not add it to the normal client box again.
         if client.class_name in config['pinned_apps']:
-            # Find and remove the pinned app button if it's present
             for button in self.pinned_box:
                 if button.app == client.class_name:
-                    button.destroy()  # Only remove if it's already present
+                    button.destroy()
 
-            # Add the new client to the pinned box
-            self.clients_box.add(DockButton(client=client))
+        dock_button = self.client_buttons.get(client.class_name)
+        if dock_button:
+            dock_button.add_client(client)
         else:
-            # Add non-pinned app to the dock
-            self.clients_box.add(DockButton(client=client))
+            dock_button = DockButton(app=client.class_name, dock=self)
+            dock_button.add_client(client)
+            self.clients_box.add(dock_button)
+            self.client_buttons[client.class_name] = dock_button
 
     def on_client_removed(self, client: HyprlandClient):
-        """Handles removing apps and re-pinning if necessary."""
         class_name = client.class_name
+        dock_button = self.client_buttons.get(class_name)
 
-        # If it's a pinned app and no instances of it remain, re-pin it
-        if class_name in config['pinned_apps']:
-            # Check if any other instance of the same app exists
-            if not any(c.class_name == class_name for c in self.clients.clients):
-                self.pinned_box.add(PinnedDockButton(app=class_name))
+        if dock_button:
+            dock_button.remove_client(client)
+            if not dock_button.clients:
+                dock_button.destroy()
+                del self.client_buttons[class_name]
+
+        if class_name in config['pinned_apps'] and not any(c.class_name == class_name for c in self.clients.clients):
+            self.pinned_box.add(PinnedDockButton(app=class_name))
+
+    def on_empty_workspace(self):
+        print(self.is_hidden)
+        self.should_hide = False
+        self.toggle_dock(True)
+
+    def on_filled_workspace(self):
+        self.should_hide = True
+        self.delay_hide()
+
+    def on_hover_enter(self):
+        self.toggle_dock(True)
+
+    def on_hover_leave(self):
+        if self.should_hide:
+            self.delay_hide()
+    
+    def show_popup(self, app, clients):
+        self.popup.initlialize_clients(app=app, clients=clients)
+        self.popup.set_visible(True)
+
+    def close_popup(self):
+        self.popup.set_visible(False)
 
     def toggle_dock(self, show):
-        """Show or hide the dock."""
+        if show == self.is_hidden:
+            self.is_hidden = not show
+            self.add_style_class("show-dock" if show else "hide-dock")
+            self.remove_style_class("hide-dock" if show else "show-dock")
+            if not show:
+                self.close_popup()
         if show:
-            if self.is_hidden:
-                self.is_hidden = False
-                self.add_style_class("show-dock")
-                self.remove_style_class("hide-dock")
-            if self.hide_id:
-                GLib.source_remove(self.hide_id)
-                self.hide_id = None
-        else:
-            if not self.is_hidden:
-                self.is_hidden = True
-                self.add_style_class("hide-dock")
-                self.remove_style_class("show-dock")
+            self.clear_hide_timeout()
 
     def delay_hide(self):
-        """Schedule hiding the dock after a short delay."""
-        if self.hide_id:
-            GLib.source_remove(self.hide_id)
+        self.clear_hide_timeout()
         self.hide_id = GLib.timeout_add(2000, self.hide_dock)
 
     def hide_dock(self):
-        """Actually hide the dock."""
-        self.toggle_dock(show=False)
-        self.hide_id = None
+        if not self.popup.get_visible():
+            self.toggle_dock(False)
+            self.hide_id = None
         return False
 
+    def clear_hide_timeout(self):
+        if self.hide_id:
+            GLib.source_remove(self.hide_id)
+            self.hide_id = None
 
-class PinnedDockButton(Box):
-    def __init__(self, app: str, **kwargs):
-        super().__init__(
-            name="dock-button",
-            orientation="v" if config['position'] in (
-                "top", "bottom") else "h",
-            **kwargs
-        )
-
-        self.app = app
-
-        self.image = Image(icon_name=self.app, icon_size=25)
-        self.button = Button(image=self.image, size=35)
+class BaseDockButton(Box):
+    def __init__(self, icon_name: str, **kwargs):
+        super().__init__(name="dock-button", orientation="v" if position in ("top", "bottom") else "h", **kwargs)
         self.label = Label("●")
-
-        self.children = [
-            self.button,
-            self.label
-        ]
-
-        self.button.connect("clicked", lambda *args: self.on_clicked())
-        self.button.connect("enter-notify-event",
-                            lambda *args: self.on_hover_enter())
-        self.button.connect("leave-notify-event",
-                            lambda *args: self.on_hover_leave())
-
-    def on_clicked(self):
-        exec_shell_command_async(f"hyprctl dispatch exec {self.app}")
+        self.image = Image(icon_name=icon_name, icon_size=30)
+        self.button = Button(image=self.image, size=40)
+        self.button.connect("enter-notify-event", lambda *args: self.on_hover_enter())
+        self.button.connect("leave-notify-event", lambda *args: self.on_hover_leave())
+        self.children = [self.button, self.label]
 
     def on_hover_enter(self):
-        """Make button icon bigger when hovered."""
-        self.image.set_pixel_size(35)
+        self.image.set_pixel_size(40)
 
     def on_hover_leave(self):
-        """Make button icon back to the default size when mouse leaves."""
-        self.image.set_pixel_size(24)
+        self.image.set_pixel_size(30)
 
+class PinnedDockButton(BaseDockButton):
+    def __init__(self, app: str, **kwargs):
+        super().__init__(icon_name=app, **kwargs)
+        self.app = app
+        self.button.connect("clicked", lambda *args: exec_shell_command_async(f"hyprctl dispatch exec {self.app}"))
 
-class DockButton(Box):
-    def __init__(self, client: HyprlandClient, **kwargs):
-        super().__init__(
-            name="dock-button",
-            orientation="v" if config['position'] in (
-                 "top", "bottom") else "h",
-            **kwargs
-        )
-        self.client = client
-
-        self.client.connect("closed", lambda *args: self.destroy())
-        self.client.connect("changed", lambda *args: self.on_changed())
-
-        self.label = Label("●")
-        self.image = Image(icon_name=self.client.class_name, icon_size=25)
-        self.button = Button(image=self.image, size=35)
-
-        self.children = [
-            self.button,
-            self.label
-        ]
-
+class DockButton(BaseDockButton):
+    def __init__(self, app: str, dock: Dock, **kwargs):
+        super().__init__(icon_name=app, **kwargs)
+        self.class_name = app
+        self.dock = dock
+        self.clients = []
         self.button.connect("clicked", lambda *args: self.on_clicked())
-        self.button.connect("enter-notify-event",
-                            lambda *args: self.on_hover_enter())
-        self.button.connect("leave-notify-event",
-                            lambda *args: self.on_hover_leave())
 
-        self.client.changed.emit()
+    def add_client(self, client: HyprlandClient):
+        self.clients.append(client)
+        client.connect("closed", lambda *args: self.remove_client(client=client))
+        client.connect("changed", lambda *args: self.on_client_changed(changed_client = client))
+        if self.dock.popup.app == self.class_name:
+            self.dock.popup.add_client(client=client)
+        self.on_client_changed()
+
+    def remove_client(self, client: HyprlandClient):
+        if client in self.clients:
+            self.clients.remove(client)
+            if self.dock.popup.app == self.class_name:
+                self.dock.popup.remove_client(client=client)
 
     def on_clicked(self):
-        exec_shell_command_async(
-            f"hyprctl dispatch focuswindow address:{self.client.address}")
+        if len(self.clients) == 1:
+            exec_shell_command_async(f"hyprctl dispatch focuswindow address:{self.clients[0].address}")
+            self.dock.close_popup()
+        else:
+            self.dock.show_popup(app=self.class_name, clients=self.clients)
 
-    def on_changed(self):
-        if self.client.focused:
+    def on_client_changed(self, changed_client: HyprlandClient = None):
+        if any(c.focused for c in self.clients):
             self.label.add_style_class("active")
         else:
             self.label.remove_style_class("active")
+        
+        if changed_client:
+            for client in self.clients:
+                if client.address == changed_client.address:
+                    client= changed_client
+        
+        if self.dock.popup.app == self.class_name:
+            self.dock.popup.update_client(client=changed_client)
 
-    def on_hover_enter(self):
-        """Make button icon bigger when hovered."""
-        self.image.set_pixel_size(35)
+class DockPopup(PopOverWindow):
+    def __init__(self,parent, pointing_to, **kwargs):
+        super().__init__(name="dock-popup",parent=parent,pointing_to=pointing_to, anchor=position,  **kwargs)
 
-    def on_hover_leave(self):
-        """Make button icon back to the default size when mouse leaves."""
-        self.image.set_pixel_size(24)
+        self.app = None
+
+        self.close_button = Button(
+            name="dock-popup-close",
+            label=close_icon,
+            v_expand=True,
+            h_align="end",
+            on_clicked=lambda *args: self._parent.close_popup()
+        )
+
+        self.clients = Box(
+            spacing=4,
+            orientation="v"
+        )
+
+        self.inner = Box(
+            spacing=8,
+            orientation="v",
+            style_classes="menu-inner",
+            style=f"margin-{position}: 60px;",
+            children=[self.close_button, self.clients]
+        )
+
+        self.add(self.inner)
+
+    def initlialize_clients(self, app: str, clients: list[HyprlandClient]):
+        self.app = app
+
+        for child in self.clients.children:
+            self.clients.remove(child)
+            child.destroy()
+
+        for client in clients:
+            button = Button(name= "dock-list-button", label=truncate(client.title, 30), on_clicked=lambda *args, client= client: self.focus_client(client=client))
+            button._client_ref = client
+            if client.focused:
+                button.add_style_class("active")
+            self.clients.add(button)
+
+    def update_client(self, client: HyprlandClient):
+        for child in self.clients.children:
+            if getattr(child, '_client_ref', None) == client:
+                child.set_label(truncate(client.title, 30))
+                if client.focused:
+                    child.add_style_class("active")
+                else:
+                    child.remove_style_class("active")
+                break
+    
+    def add_client(self, client: HyprlandClient):
+        button = Button(name= "dock-list-button", label=truncate(client.title, 30), on_clicked=lambda *args: self.focus_client(client=client))
+        button._client_ref = client
+        if client.focused:
+            button.add_style_class("active")
+        self.clients.add(button)
+
+    def remove_client(self,  client: HyprlandClient):
+        for child in self.inner.children:
+            if getattr(child, '_client_ref', None) == client:
+                self.clients.remove(child)
+                child.destroy()
+
+    def focus_client(self, client: HyprlandClient):
+        exec_shell_command_async(f"hyprctl dispatch focuswindow address:{client.address}")
+        self._parent.close_popup()
